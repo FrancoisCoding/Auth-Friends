7dd970bc9fd43cf31f371ff4c0bf67891f07ab3d
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-20 14:33:15 -0400

    Created form to add friend

--- friends/src/components/Friends/Friends.js
+++ friends/src/components/Friends/Friends.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { axiosWithAuth } from "../utils/axiosWithAuth";
+import axiosWithAuth from "../utils/axiosWithAuth";
+import AddFriend from "./AddFriend";
 
 const Friends = () => {
   const [friends, setFriends] = useState([]);
-  useEffect(getData(), []);
+  useEffect(() => getData(), []);
   function getData() {
     axiosWithAuth()
       .get("http://localhost:5000/api/friends")
@@ -12,9 +13,11 @@ const Friends = () => {
       })
       .catch(err => console.log(err.response));
   }
+  console.log("list of friends", friends);
   return (
     <div>
       <h1>Friends</h1>
+      <AddFriend />
     </div>
   );
 };
